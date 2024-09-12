372f9d2fb0e966c5f7763d815b4ed2f71ddc7507
Soneta <sopha.sts86@gmail.com>
2024-09-12 10:57:25 +0300

    Add database

---
+++ Infrastucture/Configurations/StudentConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Configurations
+{
+    internal class StudentConfigurations : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.HasOne(s => s.Basket).WithOne(b => b.Student).HasForeignKey<Student>(s => s.BasketId);
+        }
+    }
+}
--- NUWEE_ZEST/Program.cs
+++ NUWEE_ZEST/Program.cs
@@ -1,11 +1,22 @@
+using System.Text.Json.Serialization;
+using Core;
+using Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+string? connStr = builder.Configuration.GetConnectionString("LocalDb");
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext(connStr);// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
