18f045ec936668cfb42f5f873084008342097b5d
Soneta <sopha.sts86@gmail.com>
2024-09-12 09:53:07 +0300

    entities

---
+++ Core/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Post { get; set; }
+        public string Password { get; set; }
+        public double Bonus { get; set; }
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+    }
+}
